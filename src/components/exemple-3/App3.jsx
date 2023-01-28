ffd261bfbfd03da8b8138c9cafcadcaf778c8aad
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-01-28 21:06:00 +0100

    Update App3.jsx

--- src/components/exemple-3/App3.jsx
+++ src/components/exemple-3/App3.jsx
@@ -1,16 +1,35 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import { Layout } from 'components/Layout';
 import { StickerForm } from './StickerForm';
 import { StickerList } from './StickerList';
 import initialStickers from '../../stickers.json';
 
+export const App3 = () => {
+  const [stickers, setStickers] = useState(initialStickers)
+  
+  const addSticker = (img, label) => {
+    setStickers(prevState => [...prevState, { id: nanoid(), img, label }])
+  }
+  const deleteSticker = (stickerId) => {
+    setStickers(prevState => prevState.stickers.filter(sticker => sticker.id !== stickerId))
+  }
+
+  return (
+   <Layout>
+     <h1>Example 3</h1>
+     <StickerForm onSubmit={addSticker} />
+     <StickerList items={stickers} onDelete={deleteSticker}/>
+   </Layout>
+ );
+ }
+ /*
 export class App3 extends Component {
   state = {
     stickers: initialStickers,
   };
   addSticker = (img, label) => {
-    console.log(img, label);
+    //console.log(img, label);
     this.setState(prevState => ({
       stickers: [...prevState.stickers, { id: nanoid(), img, label }],
     }));
@@ -32,3 +51,4 @@ export class App3 extends Component {
     );
   }
 }
+*/
